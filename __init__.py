632eed83b0633996435101f4c2a8c18195a385f8
Klabundo <jg.andersch@gmail.com>
2025-12-14 12:55:34 +0100

    fixes

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from .PromptWeaverNode import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 
 print("🟢 PromptWeaver Node: Loaded successfully!")
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
